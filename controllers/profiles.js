3a87561374f9edbfa5e57653e06cccf44440f4dd
jordinhuang <jordinhuang@gmail.com>
2022-06-29 15:04:27 -0400

    change profile controller function

--- controllers/profiles.js
+++ controllers/profiles.js
@@ -12,8 +12,93 @@ function show(req, res){
     })
 }
 
+// function sessionIndex(req, res){
+//     Profile.find({})
+//     .then(routines => {
+//         res.render('routines/index', {
+//             title: 'All Workouts',
+//             routines
+//         })
+//     })
+// }
+//Create new workout
+function newSession(req, res){
+    Profile.findById(req.user.profile)
+    .then( profile => {
+        res.render('profiles/sessions/new', {
+            title: 'Add',
+            sessions: profile.sessions
+        })
+    })
+}
 
+function createSession(req, res){
+    for (let key in req.body) {
+        if (req.body[key] === '') delete req.body[key]
+    }
+    Profile.findById(req.user.profile)
+    .then(profile => {
+        console.log(profile)
+        profile.sessions.push(req.body)
+        profile.save()
+        res.redirect(`/profile/${req.user.profile._id}/session/new`)
+    })
+    .catch(err => {
+        console.log(err)
+    })
+}
+
+function deleteSession(req, res){
+    Profile.findById(req.user.profile)
+    .then(profile => {
+        profile.sessions.remove({_id:req.params.sessionId})
+        profile.save()
+        .then(() => {
+            res.redirect(`/profile/${req.user.profile._id}/session/new`)
+    })
+    .catch(err => {
+        console.log(err)
+        res.redirect(`/profile/${req.user.profile._id}/session/new`)
+    })
+})
+}
+
+function createSet(req, res){
+    Routine.findById(req.params.id)
+    .then(routine => {
+        routine.sets.push(req.body)
+        routine.save()
+        .then(() => {
+            res.redirect('/routines/new')
+        })
+    })
+    .catch(err => {
+        console.log(err)
+        res.redirect('/routines/new')
+    })
+}
+
+function deleteSet(req, res){
+    Routine.findById(req.params.routineId)
+    .then(routine => {
+        routine.sets.remove({_id:req.params.setId})
+        routine.save()
+        .then(() => {
+            res.redirect('/routines/new')
+        })
+    })
+    .catch(err => {
+        console.log(err)
+        res.redirect('/routines/new')
+    })
+}
 
 export{
-    show
+    show,
+    // sessionIndex,
+    newSession as new,
+    createSession,
+    deleteSession as delete,
+    createSet,
+    deleteSet
 }
--- routes/profiles.js
+++ routes/profiles.js
@@ -5,9 +5,9 @@ import * as profilesCtrl from '../controllers/profiles.js'
 const router = Router()
 
 router.get('/:id', isLoggedIn, profilesCtrl.show)
-router.get('/session/new', isLoggedIn, profilesCtrl.new)
-router.post('/session', isLoggedIn, profilesCtrl.create)
-router.delete('/session/:id', isLoggedIn, profilesCtrl.delete)
+router.get('/:id/session/new', isLoggedIn, profilesCtrl.new)
+router.post('/session', isLoggedIn, profilesCtrl.createSession)
+router.delete('/session/:sessionId', isLoggedIn, profilesCtrl.delete)
 router.post('/session/:id/sets', isLoggedIn, profilesCtrl.createSet)
 router.delete('/:id/session/:sessionId/sets/:setId', isLoggedIn, profilesCtrl.deleteSet)
 
